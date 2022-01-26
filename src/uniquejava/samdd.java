7161e72420e6b647f6cd91540c036a9811a83575
Mugundhanp2497 <mugundhanp2497@gmail.com>
2022-01-26 18:48:52 +0530

    work here

--- src/uniquejava/samdd.java
+++ src/uniquejava/samdd.java
@@ -1,5 +1,13 @@
 package uniquejava;
 
-public class samdd {
+public class samdd 
+{
+	public static void main(String[] args) 
+	
+	{
+		System.out.println("one two three");
+		System.out.println("asdf");
+		
+	}
 
 }
